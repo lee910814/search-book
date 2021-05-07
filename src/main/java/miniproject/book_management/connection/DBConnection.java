9d8455eea7245731edcb7cb6225abcc2c47ff17a
slo-th <zmtq05@gmail.com>
2021-05-07 19:59:31 +0900

    Add db Connection

---
+++ src/main/java/miniproject/book_management/connection/DBConnection.java
@@ -0,0 +1,16 @@
+package miniproject.book_management.connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() throws NamingException, SQLException {
+        Context context = new InitialContext();
+        DataSource ds = (DataSource) context.lookup("java:comp/env/jdbc/mysql");
+        return ds.getConnection();
+    }
+}
