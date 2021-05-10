400446d79097dc74d19cdfda9e185ba7661b8d0e
slo-th <zmtq05@gmail.com>
2021-05-10 22:02:58 +0900

    Implement BookDTO

---
+++ src/main/java/miniproject/book_management/dto/BookDTO.java
@@ -0,0 +1,40 @@
+package miniproject.book_management.dto;
+
+public class BookDTO {
+    private Long id;
+    private String name;
+    private String publisher;
+    private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
