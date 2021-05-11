0ddc4a87ba8335b18534c756321fa4aacd3c0e39
slo-th <zmtq05@gmail.com>
2021-05-11 17:02:28 +0900

    Add bookList to BookDao

--- src/main/java/miniproject/book_management/dao/BookDao.java
+++ src/main/java/miniproject/book_management/dao/BookDao.java
@@ -6,7 +6,10 @@ import miniproject.book_management.dto.BookDto;
 import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookDao {
     public boolean registerBook(BookDto bookDto) {
@@ -22,5 +25,31 @@ public class BookDao {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public List<BookDto> bookList() {
+        List<BookDto> list = new ArrayList<>();
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement("select * from book")
+        ) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    BookDto dto = mappingBook(rs);
+                    list.add(dto);
+                }
+            }
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private BookDto mappingBook(ResultSet rs) throws SQLException {
+        BookDto dto = new BookDto();
+        dto.setId(rs.getLong("book_id"));
+        dto.setName(rs.getNString("name"));
+        dto.setPublisher(rs.getNString("publisher"));
+        dto.setAuthor(rs.getNString("author"));
+        return dto;
     }
 }
