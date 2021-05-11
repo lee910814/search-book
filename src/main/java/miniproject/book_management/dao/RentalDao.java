981d0cfc2a5dbc1e8f6cc6312bac88326d5ac8d2
slo-th <zmtq05@gmail.com>
2021-05-11 19:36:41 +0900

    Add findAll to RentalDao

--- src/main/java/miniproject/book_management/dao/RentalDao.java
+++ src/main/java/miniproject/book_management/dao/RentalDao.java
@@ -6,7 +6,10 @@ import miniproject.book_management.dto.RentalDto;
 import javax.naming.NamingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RentalDao {
     public boolean save(RentalDto rentalDto) {
@@ -23,5 +26,32 @@ public class RentalDao {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public List<RentalDto> findAll() {
+        List<RentalDto> list = new ArrayList<>();
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement("select * from rental")
+        ) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    RentalDto dto = mappingRental(rs);
+                    list.add(dto);
+                }
+            }
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private RentalDto mappingRental(ResultSet rs) throws SQLException {
+        RentalDto dto = new RentalDto();
+        dto.setId(rs.getLong("rental_id"));
+        dto.setMemberId(rs.getLong("member_id"));
+        dto.setBookId(rs.getLong("book_id"));
+        dto.setTermDay(rs.getInt("term_day"));
+        dto.setOverdueDay(rs.getInt("overdue_day"));
+        return dto;
     }
 }
