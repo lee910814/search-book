a90b294d5e515f269ba28e227a3765d7392bbc62
slo-th <zmtq05@gmail.com>
2021-05-13 23:24:19 +0900

    Implement findById
    findById returns List<RentalBookDto>

--- src/main/java/miniproject/book_management/dao/RentalDao.java
+++ src/main/java/miniproject/book_management/dao/RentalDao.java
@@ -1,6 +1,7 @@
 package miniproject.book_management.dao;
 
 import miniproject.book_management.connection.DBConnection;
+import miniproject.book_management.dto.RentalBookDto;
 import miniproject.book_management.dto.RentalDto;
 
 import javax.naming.NamingException;
@@ -15,12 +16,10 @@ public class RentalDao {
     public boolean save(RentalDto rentalDto) {
         try (Connection con = DBConnection.getConnection();
              PreparedStatement ps = con.prepareStatement(
-                     "insert into rental(member_id, book_id, term_day, overdue_day) VALUES (?, ?, ?, ?)"
+                     "insert into rental(member_id, book_id) VALUES (?, ?)"
              )) {
             ps.setLong(1, rentalDto.getMemberId());
             ps.setLong(2, rentalDto.getBookId());
-            ps.setInt(3, rentalDto.getTermDay());
-            ps.setInt(4, rentalDto.getOverdueDay());
             return ps.executeUpdate() > 0;
         } catch (SQLException | NamingException e) {
             e.printStackTrace();
@@ -50,8 +49,41 @@ public class RentalDao {
         dto.setId(rs.getLong("rental_id"));
         dto.setMemberId(rs.getLong("member_id"));
         dto.setBookId(rs.getLong("book_id"));
-        dto.setTermDay(rs.getInt("term_day"));
-        dto.setOverdueDay(rs.getInt("overdue_day"));
+        dto.setExpireDate(rs.getDate("expire_date"));
+        return dto;
+    }
+
+    public List<RentalBookDto> findById(Long id) {
+        List<RentalBookDto> list = new ArrayList<>();
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(
+                     "select * from" +
+                     "(select rental_id, r.book_id, member_id, expire_date, name, publisher, author " +
+                     "from rental r INNER JOIN book b on r.book_id = b.book_id) as rb " +
+                     "where rb.member_id = ?")
+        ) {
+            ps.setLong(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    RentalBookDto dto = mappingRentalBook(rs);
+                    list.add(dto);
+                }
+            }
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private RentalBookDto mappingRentalBook(ResultSet rs) throws SQLException {
+        RentalBookDto dto = new RentalBookDto();
+        dto.setId(rs.getLong("rental_id"));
+        dto.setBookId(rs.getLong("book_id"));
+        dto.setMemberId(rs.getLong("member_id"));
+        dto.setExpireDate(rs.getDate("expire_date"));
+        dto.setBookName(rs.getString("name"));
+        dto.setPublisher(rs.getString("publisher"));
+        dto.setAuthor(rs.getString("author"));
         return dto;
     }
 
