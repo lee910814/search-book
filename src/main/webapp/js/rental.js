2507b34cb64524d3fd1d07c66ea471d5f16ec758
slo-th <zmtq05@gmail.com>
2021-05-13 23:25:23 +0900

    Implement return ajax function

--- src/main/webapp/js/rental.js
+++ src/main/webapp/js/rental.js
@@ -6,11 +6,51 @@ $(document).ready(() => {
             data: {
                 id: e.target.value
             }
-        // todo : Do not use alert
-        }).done(() => {
-            alert("대여되었습니다.")
-        }).fail(() => {
-            alert("오류 발생")
-        })
+        }).complete({message: "대여되었습니다."}, reply)
+
+    })
+
+    $(".return-button").click((e) => {
+        $.ajax({
+            url: "return.jsp",
+            type: "post",
+            data: {
+                id: e.target.value
+            }
+        }).complete({message: "반납되었습니다."}, reply)
     })
 })
+
+/**
+ * Ajax complete callback function
+ * @param resp ajax response
+ * @param message message to be sent to user
+ */
+function reply(resp, message) {
+    const status = resp.status;
+    if (status !== 200) {
+        const msg = getErrMsg(status);
+        alert(msg);
+        return;
+    }
+
+    alert(message);
+}
+
+/**
+ * Return error message
+ * @param statusCode HTTP response status code
+ * @returns {string} Error message
+ */
+function getErrMsg(statusCode) {
+    switch (statusCode) {
+        case 400:
+            return "잘못된 요청입니다.";
+        case 401:
+            return "로그인이 필요합니다.";
+        case 405:
+            return "method not allowed";
+        default:
+            return `Unhandled Code: ${statusCode}`;
+    }
+}
